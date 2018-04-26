b21b91c3cffdc4ad3215ae01286bdbb356c54d5e
andrey2503 <andreytorresvega@gmail.com>
2018-04-25 21:09:43 -0600

    Control izquierda derecha del cubo

---
+++ Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour {
+
+	public int indiceEscena =  0 ;
+	// public bool timer = false;
+	public bool timer = true;	
+	public float tiempoEspera = 3f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(timer){
+			StartCoroutine (SceneSwitcher ());
+		}
+	}
+
+	public void CambiarEscenaManual(){
+		SceneManager.LoadScene (indiceEscena);
+	}
+
+	private IEnumerator SceneSwitcher(){
+		yield return new WaitForSeconds (tiempoEspera);
+		Debug.Log ("Cambio de scene");
+		SceneManager.LoadScene (indiceEscena);
+	}
+}
