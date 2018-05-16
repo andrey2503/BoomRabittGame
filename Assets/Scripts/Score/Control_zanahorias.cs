aee214ae9b3a0dd0a574d495399ae7e1470d7b2c
andrey2503 <andreytorresvega@gmail.com>
2018-05-16 00:03:21 -0600

    Mecanica puntos

---
+++ Assets/Scripts/Score/Control_zanahorias.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_zanahorias : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnTriggerEnter(Collider meta){
+		if(meta.gameObject.tag=="Player"){
+			Debug.Log ("Punto");
+			//Player_Inputs.instance.activo = false;
+			Destroy(this.gameObject);
+			Puntos_control.instance.sumarPunto ();
+		}
+	}// OnTriggerEnter
+}
---
+++ Assets/Scripts/Score/Puntos_control.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puntos_control : MonoBehaviour {
+
+	public static Puntos_control instance;
+	public int puntos=0;
+	// Use this for initialization
+	void Start () {
+		instance = this; 
+	}
+
+	public void sumarPunto(){
+		puntos++;
+		Debug.Log (puntos);
+	}
+}
