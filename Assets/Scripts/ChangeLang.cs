b0b25410612d9201891e333a3000514d7097197c
andrey2503 <andreytorresvega@gmail.com>
2018-05-25 20:07:47 -0600

    multi idioma

---
+++ Assets/Scripts/ChangeLang.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLang : MonoBehaviour {
+
+	// Use this for initialization
+	public void changeLang(string fileName) {
+		GameControl.instance.LoadLocalizedText(fileName);
+	}
+}
