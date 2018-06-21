78540257042266884e8d14143d6cc58baab62cb6
andrey2503 <andreytorresvega@gmail.com>
2018-06-21 15:02:03 -0600

    juego de camara plataforma doble

--- Assets/Scripts/Player/Camara_controller.cs
+++ Assets/Scripts/Player/Camara_controller.cs
@@ -7,18 +7,25 @@ public class Camara_controller : MonoBehaviour {
 	// Use this for initialization
 	public int posy=4;
 	public float velocidad=1.0f;
+	public float velocidadZoom=1.0f;
 	public GameObject Main_Camera;
+	public float originaly,originalz;
 	void Start () {
 		instance = this;
+		originaly = Main_Camera.GetComponent<Transform> ().position.y;
+		originalz = Main_Camera.transform.position.z;
 	}// fin del start
 
 
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 destino = new Vector3 (Player_engine.instance.getPosX (), Main_Camera.GetComponent<Transform> ().position.y, Main_Camera.transform.position.z);
-		//Main_Camera.GetComponent<Transform> ().position
-		//= new Vector3(Player_engine.instance.getPosX (),Main_Camera.GetComponent<Transform>().position.y,Main_Camera.transform.position.z);
-		Main_Camera.GetComponent<Transform> ().position=Vector3.Lerp (Main_Camera.GetComponent<Transform> ().position,destino,Time.deltaTime*velocidad);
+		if (Player_engine.instance.getPosY () > 8) {
+			Vector3 destino = new Vector3 (Player_engine.instance.getPosX (), Player_engine.instance.getPosY ()+4, -18f);
+			Main_Camera.GetComponent<Transform> ().position=Vector3.Lerp (Main_Camera.GetComponent<Transform> ().position,destino,Time.deltaTime*velocidad);
+		} else {
+			Vector3 destino = new Vector3 (Player_engine.instance.getPosX (),originaly ,originalz);
+			Main_Camera.GetComponent<Transform> ().position=Vector3.Lerp (Main_Camera.GetComponent<Transform> ().position,destino,Time.deltaTime*velocidad);
+		}
 	}// fin de update
 }
